5f07176b0ff664bc8041c613aab682963d868be2
emekadefirst <victorchibuogwu33@gmail.com>
2025-02-19 11:56:31 +0100

    templates

--- main/leaseapp/urls.py
+++ main/leaseapp/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.main, name="Landing page"),
     path("home", views.home, name="Home page"),
-    path("detail", views.detail, name="detail page"),
+    path("detail/<int:id>", views.detail, name="detail page"),
+    path("filter", views.vehicle_filter, name="filter")
    
 ]
--- main/leaseapp/views.py
+++ main/leaseapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Vehicle, EngineType, VehicleType
 
 # Create your views here.
 def main(request):
@@ -8,8 +9,31 @@ def main(request):
 def base(request):
     return render(request, 'base.html')
 
-def detail(request):
-    return render(request, 'detail.html')
+
+def detail(request, id):
+    vehicle = get_object_or_404(Vehicle, id=id)
+    return render(request, 'detail.html', {"vehicle": vehicle})
 
 def home(request):
-    return render(request, 'index.html')
+    vehicles = Vehicle.objects.all()
+    engintype = EngineType.objects.all()
+    vehicletype = VehicleType.objects.all()
+    return render(request, 'index.html', {"vehicles": vehicles, "engintype": engintype, "vehicletype": vehicletype})
+
+
+def vehicle_filter(request):
+    etype = request.GET.get('etype')
+    type = request.GET.get('type')
+    year = request.GET.get('year')
+    brand = request.GET.get('brand')
+    filters = {}
+    if etype:
+        filters["engine_type"] = etype
+    if type:
+        filters["typevehicle_type"] = type
+    if year:
+        filters["year"] = year
+    if brand:
+        filters["brand"] = year
+    vehicles = Vehicle.objects.filter(**filters)
+    return render(request, 'index.html', {'vehicle': vehicles})
