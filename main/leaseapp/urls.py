3a11d356e0d06f10ef5ae630bb5bf4be6861666a
emekadefirst <victorchibuogwu33@gmail.com>
2025-02-24 11:59:15 +0100

    24/02/2025

--- main/leaseapp/urls.py
+++ main/leaseapp/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path("", views.main, name="Landing page"),
     path("home", views.home, name="Home page"),
     path("detail/<int:id>", views.detail, name="detail page"),
-    path("filter", views.vehicle_filter, name="filter")
-   
+    path("filter", views.vehicle_filter, name="filter"),
+    path("login", views.login_view, name="login"),
+    path("signup", views.signup, name="signup"),
+    path('logout', views.logout_user, name="logout"),
 ]
--- main/leaseapp/views.py
+++ main/leaseapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Vehicle, EngineType, VehicleType
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, logout
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
 def main(request):
@@ -21,19 +24,56 @@ def home(request):
     return render(request, 'index.html', {"vehicles": vehicles, "engintype": engintype, "vehicletype": vehicletype})
 
 
+from django.shortcuts import render
+from .models import Vehicle, EngineType, VehicleType  # Import missing models
+
+
 def vehicle_filter(request):
-    etype = request.GET.get('etype')
-    type = request.GET.get('type')
-    year = request.GET.get('year')
-    brand = request.GET.get('brand')
-    filters = {}
+    etype = request.GET.get("etype")
+    vtype = request.GET.get("type")
+
+    vehicles = Vehicle.objects.all()
+
     if etype:
-        filters["engine_type"] = etype
-    if type:
-        filters["typevehicle_type"] = type
-    if year:
-        filters["year"] = year
-    if brand:
-        filters["brand"] = year
-    vehicles = Vehicle.objects.filter(**filters)
-    return render(request, 'index.html', {'vehicle': vehicles})
+        vehicles = vehicles.filter(engine_type__name=etype)  
+    if vtype:
+        vehicles = vehicles.filter(vehicle_type__name=vtype)
+    engintype = EngineType.objects.all()
+    vehicletype = VehicleType.objects.all()
+
+    return render(
+        request,
+        "index.html",
+        {"vehicles": vehicles, "engintype": engintype, "vehicletype": vehicletype},
+    )
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect("Home page")
+        else:
+            return redirect("login")
+    return render(request, 'login.html')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        if password1 == password2:
+            user = User.objects.create(username=username, email=email)
+            user.set_password(password1)
+            user.save()
+            auth_login(request, user)
+            return redirect("/home")
+    return render(request, 'signup.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("/login")
